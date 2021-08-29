7061e06f14495fff285d18cd070b41a00b3b8069
luiz pedro <luizpedro.reech@hotmail.com>
2021-08-29 01:10:41 -0300

    Commit inicial

---
+++ src/main/java/com/empatia/EmpatiaApplication.java
@@ -0,0 +1,14 @@
+package com.empatia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpatiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpatiaApplication.class, args);
+	}
+
+}
+
